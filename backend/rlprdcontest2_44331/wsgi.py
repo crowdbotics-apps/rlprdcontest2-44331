b2d7d35983f820975a94b76e2bbb1e0bfa0d7b47
Crowdbotics <team@crowdbotics.com>
2023-11-03 22:14:14 +0000

    Initial commit

---
+++ backend/rlprdcontest2_44331/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for rlprdcontest2_44331 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "rlprdcontest2_44331.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rlprdcontest2-44331",
+  url: "https://rlprdcontest2-44331.botics.co",
+  api: "https://rlprdcontest2-44331.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
